85777b85eb64be746633a62bc832251749eb1a09
Alex <maisilex7@gmail.com>
2020-10-03 15:53:06 +0300

    upd

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,10 @@
 
 import { InertiaApp } from '@inertiajs/inertia-svelte'
 
+import { InertiaProgress } from '@inertiajs/progress'
+
+InertiaProgress.init()
+
 const app = document.getElementById('app')
 
 new InertiaApp({
