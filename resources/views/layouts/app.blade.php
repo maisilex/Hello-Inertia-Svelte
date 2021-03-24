bae5de96e6debcb8d79d0ec7c4cd89d6da187df3
Alex <maisilex7@gmail.com>
2021-03-24 17:51:19 +0300

    Add Inertia to Server-Side

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,15 +1,12 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-  <title>Document</title>
-</head>
-<body>
-  <main>
-    <h1>Hey there!!!</h1>
-  </main>
-</body>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+    <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
+    <script src="{{ mix('/js/app.js') }}" defer></script>
+  </head>
+  <body>
+    @inertia
+  </body>
 </html>
